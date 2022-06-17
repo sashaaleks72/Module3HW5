0f1df01b3d57a99fafca7a4c2619ee7732978f32
sashaaleks72 <sashaaleks72@gmail.com>
2022-06-17 19:10:58 +0300

    Implemented 3 async methods. Two of them asynchronously read text from files hello.txt or world.txt and another one returns concatinated text from those two methods. That's all.

---
+++ Module3HW5/Module3HW5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Module3HW5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            AsyncMethods asyncMethods = new AsyncMethods();
+            string helloWorld = asyncMethods.GetHelloWorldTextAsync().Result;
+
+            Console.WriteLine(helloWorld);
+        }
+    }
+}
